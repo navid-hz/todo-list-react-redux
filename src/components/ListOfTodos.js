fbe1a8a76f356f6dab9e737445ca468485d1e5a3
Navid Hosseini <hosseini.zari@gmail.com>
2023-12-21 16:41:21 +0100

    Emty list Style and logic

--- src/components/ListOfTodos.js
+++ src/components/ListOfTodos.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import style from './ListOfTodos.module.css'
+import emptyList from '../assets/images/empty-list-removebg-preview.png'
+import { Link } from 'react-router-dom';
 
 function ListOfTodos() {
-  return (
-    <div>List Of Todos</div>
-  )
-}
+  const loadTodos = [];
 
-export default ListOfTodos
+  return (
+    <div className={style.tlContainer}>
+      {loadTodos.length > 0 ? (
+      //show list of todos
+      console.log('show list of todos')
+      ):(
+      //show empty list image
+      <div className={style.tlEmty}>
+        <img src={emptyList} alt='empty list' />
+        <p>There is no todo yet</p>
+        <Link to='/add-todo'>
+          <button>Let's get to it!</button>
+        </Link>
+      </div>
+      )} 
+    </div>
+  );
+};
+
+
+export default ListOfTodos;
